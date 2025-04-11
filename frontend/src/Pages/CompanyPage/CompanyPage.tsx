691a5907d4d38e659f3b053f8ec922cfa222c5d3
Koustav Mishra <62167082+KKMishra1999@users.noreply.github.com>
2025-04-11 23:35:49 +0530

    useEffect to load comapany details

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,10 +1,40 @@
-import React, { JSX } from 'react'
+import React, { JSX, useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { getCompanyProfile } from '../../api';
+import { CompanyProfile } from '../../company';
 
 interface Props {}
 
 const CompanyPage: React.FC<Props> = (props: Props): JSX.Element => {
+  let {ticker} = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
+  const [warning, setWarning] = useState<string>('');
+
+  useEffect(() => { 
+    const getProfileInit = async() => {
+      const result = await getCompanyProfile(ticker!);      
+      if(typeof result === 'string'){
+        setWarning(result);
+      }
+      else{
+        setCompany(result?.data[0]);        
+      }
+    }
+    getProfileInit();
+  }, [ticker])
+  
+
   return (
-    <div>CompanyPage</div>
+    <>
+      {warning ?? <h1 className='text text-red-500'>warning</h1>}
+      <div>
+        {
+          company ? <h1>{ company.companyName}</h1>
+          : <h1>Company not found!</h1>
+        }
+      </div>
+    </>
+
   )
 }
 
--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { CompanySearch } from "./company";
+import { CompanyProfile, CompanySearch } from "./company";
 
 interface SearchResponse{
     data: CompanySearch[];
@@ -23,5 +23,23 @@ export const searchCompanies = async(query: string) => {
         console.log("Unexpected error: ", error);
         return "An unexpected error occured";
        }
+    }
+}
+
+export const getCompanyProfile = async(query: string) => {
+    try{
+        const data = await axios.get<CompanyProfile[]>(
+            `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${import.meta.env.VITE_API_KEY}`
+        );
+        return data;
+    }catch (error) {
+        if(axios.isAxiosError(error)){
+         console.log("Error message: ", error.message);
+         return error.message;
+        }
+        else{
+         console.log("Unexpected error: ", error);
+         return "An unexpected error occured";
+        }
     }
 }
